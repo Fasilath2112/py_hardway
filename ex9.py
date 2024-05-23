ae4175f35e2d022b3abeb5e303da588628521333
Fasilath K S <fasilathsirajudheen2112@gmail.com>
2024-05-23 10:17:24 +0530

    Printing printing printing

---
+++ ex9.py
@@ -0,0 +1,7 @@
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nmar\napr\nmay\njun\njul\naug"
+print("here are the days:",days)
+print("here are the months:",months)
+print("""
+there's something going on her.
+with the three double quotes""")
