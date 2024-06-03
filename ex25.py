e522387afbc329db72a0c7726000a466ca228996
Fasilath K S <fasilathsirajudheen2112@gmail.com>
2024-06-03 13:18:44 +0530

    Even More Practice

--- ex25.py
+++ ex25.py
@@ -1,11 +1,11 @@
 def break_words (stuff):
     """This function will break up words for us."""
-    words= stuff.split('')
+    words= stuff.split(' ')
     return words
 
 def sort_words (words):
     """Sorts the words."""
-    return sorted (words)
+    return sorted(words)
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
@@ -20,7 +20,7 @@ def print_last_word(words):
 def sort_sentence (sentence):
     """Takes in a full sentence and returns the sorted words...."""
     words=break_words(sentence)
-    return sort_words (words)
+    return sort_words(words)
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
